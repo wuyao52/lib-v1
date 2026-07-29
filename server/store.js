f3944f090497ddb2d88f8d8647347df9f4feebf0
wuyao52 <wuyao52@users.noreply.github.com>
2026-07-29 19:03:57 +0800

    Create store.js

---
+++ server/store.js
@@ -0,0 +1,49 @@
+import { mkdir, readFile, rename, writeFile } from 'node:fs/promises';
+import { dirname } from 'node:path';
+
+const EMPTY_DATABASE = {
+  users: [],
+  sessions: [],
+  emailVerifications: [],
+  imageCaptchas: [],
+  skills: [],
+};
+
+export class JsonDatabase {
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.data = structuredClone(EMPTY_DATABASE);
+    this.writeQueue = Promise.resolve();
+  }
+
+  async init() {
+    await mkdir(dirname(this.filePath), { recursive: true });
+    try {
+      const parsed = JSON.parse(await readFile(this.filePath, 'utf8'));
+      this.data = { ...structuredClone(EMPTY_DATABASE), ...parsed };
+    } catch (error) {
+      if (error.code !== 'ENOENT') throw error;
+      await this.persist();
+    }
+    return this;
+  }
+
+  read(collection) {
+    return this.data[collection];
+  }
+
+  async mutate(mutator) {
+    const result = mutator(this.data);
+    await this.persist();
+    return result;
+  }
+
+  async persist() {
+    this.writeQueue = this.writeQueue.then(async () => {
+      const tempPath = `${this.filePath}.tmp`;
+      await writeFile(tempPath, JSON.stringify(this.data, null, 2), 'utf8');
+      await rename(tempPath, this.filePath);
+    });
+    return this.writeQueue;
+  }
+}
